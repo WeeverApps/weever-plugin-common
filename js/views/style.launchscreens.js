70ef2954e7eac7e561e9d4ca0aef5fc42806968e
Matt Grande <matt@weeverapps.com>
2013-09-23 11:02:17 -0400

    Moving to the more specific 'set_launchscreen' method. [refs #3667]

--- js/views/style.launchscreens.js
+++ js/views/style.launchscreens.js
@@ -18,24 +18,22 @@ wxApp = wxApp || {};
 
             $('#save_launch_screen').html('Saving...');
 
-            // The 'set_design' method of Open API is kinda strange... It 
+            // The 'design' methods of Open API is kinda strange... It 
             // expects top-level params to be JSON objects, and items within 
             // the top-level params to be string representations of JSON 
             // objects... Therefore, we have to 'stringify' the inner params.
             var innerParams = JSON.stringify( {
-                launchscreen: {
-                    phone: this.cleanUrl( $('#wx-phone_load_live').attr('src') ),
-                    tablet: this.cleanUrl( $('#wx-tablet_load_live').attr('src') ),
-                    tablet_landscape: this.cleanUrl( $('#wx-tablet_landscape_load_live').attr('src') )
-                }
+                phone: this.cleanUrl( $('#wx-phone_load_live').attr('src') ),
+                tablet: this.cleanUrl( $('#wx-tablet_load_live').attr('src') ),
+                tablet_landscape: this.cleanUrl( $('#wx-tablet_landscape_load_live').attr('src') )
             } );
             var params = {
-                design: innerParams
+                launchscreen: innerParams
             };
 
             // set_launchscreen
-            wx.makeApiCall('design/set_design', params, function(data) {
-                $('#save_logo_design').html('Saved!');
+            wx.makeApiCall('design/set_launchscreen', params, function(data) {
+                $('#save_launch_screen').html('Saved!');
                 // Wait half a second, then refresh the preview
                 // (The half-second helps ensure the server is synced)
                 setTimeout( function() { wx.refreshAppPreview(); }, 500);
