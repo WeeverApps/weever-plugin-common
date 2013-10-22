0102baaeae9bad3bcf724c1cb83458b02db4958b
Matt Grande <matt@weeverapps.com>
2013-10-22 16:30:28 -0400

    Remove save button in favour of ajaxy for logo design. [refs #3750 and spent 2 hours on #3750]

--- js/views/style.logodesign.js
+++ js/views/style.logodesign.js
@@ -5,8 +5,9 @@ wxApp = wxApp || {};
     wxApp.LogoDesign = Backbone.View.extend({
         el: '#logo_design',
         events: {
-            'click #save_logo_design': 'save',
-            'change #titlebarSource': 'dropDownChange'
+            'change #titlebarSource': 'dropDownChange',
+            'change .color': 'colorChange',
+            'change .logo-design': 'logoChange'
         },
 
         initialize: function() {
@@ -15,72 +16,7 @@ wxApp = wxApp || {};
             this.$('.content').html( this.tpl( this.model.toJSON() ) );
         },
 
-        save: function() {
-            console.log('logo design: save clicked');
-
-            $('#save_logo_design').html('Saving...');
-
-            var requestOneSuccess = false;
-            var requestTwoSuccess = false;
-
-            // Request 1: Update WP Options (the colours)
-            $.ajax({
-                type: "POST",
-                url: ajaxurl,
-                data: { 
-                    action: 'ajaxSaveTheme',
-                    nonce: $('input#nonce').val(),
-                    main_titlebar_color: $('#main_titlebar_color').val(),
-                    main_titlebar_text_color: $('#main_titlebar_text_color').val(),
-                    subtab_color: $('#subtab_color').val(),
-                    subtab_text_color: $('#subtab_text_color').val()
-                },
-                success: function(msg) {
-                    console.log('OK');
-                    requestOneSuccess = true;
-
-                    if ( requestTwoSuccess ) {
-                        $('#save_logo_design').html('Saved!');
-                        // Wait half a second, then refresh the preview
-                        // (The half-second helps ensure the server is synced)
-                        setTimeout( function() { wx.refreshAppPreview(); }, 500);
-                    }
-                },
-                error: function(v, msg) {
-                    //alert(v);
-                    alert(msg);
-                }
-            });
-
-            wxApp.design.get('titlebar').html  = $('#titlebar_html').val();
-            wxApp.design.get('titlebar').text  = $('#titlebar_title').val();
-            wxApp.design.get('titlebar').type  = $('#titlebarSource').val();
-            wxApp.design.get('titlebar').image = $('#titlebar_logo_live').attr('src');
-
-            // Request 2: Update Weever options (the titlebar)
-            //
-            // The 'design' methods of Open API is kinda strange... It 
-            // expects top-level params to be JSON objects, and items within 
-            // the top-level params to be string representations of JSON 
-            // objects... Therefore, we have to 'stringify' the inner params.
-            var innerParams = JSON.stringify( wxApp.design.get('titlebar') );
-            var params = { titlebar: innerParams };
-
-            wx.makeApiCall('design/set_titlebar', params, function(data) {
-                requestTwoSuccess = true;
-
-                if ( requestOneSuccess ) {
-                    $('#save_logo_design').html('Saved!');
-                    // Wait half a second, then refresh the preview
-                    // (The half-second helps ensure the server is synced)
-                    setTimeout( function() { wx.refreshAppPreview(); }, 500);
-                }
-            });
-        },
-
         dropDownChange: function() {
-            console.log('logo design: drop down changed');
-
             switch($('#titlebarSource').val()) {
                 case 'text':
                     $('#logoText').show();
@@ -98,6 +34,82 @@ wxApp = wxApp || {};
                     $('#logoImage').hide();
                     break;
             }
+            this.logoChange( this );
+        },
+
+        colorChange: function(e) {
+            var me = this;
+            var txt = $(e.currentTarget);
+            var id = txt.attr('id');
+            var val = txt.val();
+            // Make sure the colour is in the form '#ffffff' rahter than just 'ffffff'
+            if (val.indexOf('#') !== 0)
+                val = '#' + val;
+
+            // Show loading gif
+            var loading_id = '#' + id + '_loading';
+            $(loading_id).show();
+            
+            var data = { 
+                    action: 'ajaxSaveTheme',
+                    nonce: $('input#nonce').val()
+            };
+            data[id] = val;
+
+            $.ajax({
+                type: "POST",
+                url: ajaxurl,
+                data: data,
+                success: function(msg) {
+                    me.hideLoadGif( id, loading_id );
+                    
+                    // Wait half a second, then refresh the preview
+                    // (The half-second helps ensure the server is synced)
+                    setTimeout( function() { wx.refreshAppPreview(); }, 500);
+                },
+                error: function(v, msg) {
+                    //alert(v);
+                    alert(msg);
+                }
+            });
+        },
+
+        logoChange: function(e) {
+            var me = this;
+            var txt = $(e.currentTarget);
+            var id = txt.attr('id');
+
+            // Show loading gif
+            var loading_id = '#' + id + '_loading';
+            $(loading_id).show();
+            
+            wxApp.design.get('titlebar').html  = $('#titlebar_html').val();
+            wxApp.design.get('titlebar').text  = $('#titlebar_title').val();
+            wxApp.design.get('titlebar').type  = $('#titlebarSource').val();
+            wxApp.design.get('titlebar').image = $('#titlebar_logo_live').attr('src');
+            
+            // The 'design' methods of Open API is kinda strange... It 
+            // expects top-level params to be JSON objects, and items within 
+            // the top-level params to be string representations of JSON 
+            // objects... Therefore, we have to 'stringify' the inner params.
+            var innerParams = JSON.stringify( wxApp.design.get('titlebar') );
+            var params = { titlebar: innerParams };
+
+            wx.makeApiCall('design/set_titlebar', params, function(data) {
+                me.hideLoadGif( id, loading_id );
+                setTimeout( function() { wx.refreshAppPreview(); }, 500);
+            });
+        },
+
+        hideLoadGif: function(id, loading_id) {
+            // Create a span that tells you the element was saved.
+            // Add it to the page for two seconds, then fade it out, then remove it from the page.
+            // Hide the loading gif
+            var post_load = id + "_done";
+            var span = "<span id=\"" + post_load + "\">Saved.</span>";
+            $(loading_id).before( span );
+            $( '#' + post_load ).delay( 2000 ).fadeOut( 1000 ).queue( function() { $(this).remove();} );
+            $(loading_id).hide();
         }
     });
 
