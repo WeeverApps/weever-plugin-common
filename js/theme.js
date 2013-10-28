810b9eddbe5fd5fd6d93cd50013814a811bc69d7
Matt Grande <matt@weeverapps.com>
2013-10-28 09:33:11 -0400

    Merging in changes from dev server (mostly Andrew's view changes)

--- js/theme.js
+++ js/theme.js
@@ -16,6 +16,7 @@
 *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 *   GNU General Public License for more details <http://www.gnu.org/licenses/>.
 *
+*	NOTE - Most (if not all) of this script can be removed.
 */
 
 /*
