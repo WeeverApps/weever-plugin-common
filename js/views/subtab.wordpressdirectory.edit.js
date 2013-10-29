6667746b73ea757d8a62f4a20d7b1f224c2d183f
Matt Grande <matt@weeverapps.com>
2013-10-29 15:03:49 -0400

    Selecting wordpress categories from the dropdowns.

--- js/views/subtab.wordpressdirectory.edit.js
+++ js/views/subtab.wordpressdirectory.edit.js
@@ -5,6 +5,13 @@ wxApp = wxApp || {};
     wxApp.WordpressDirectorySubTabEditView = wxApp.SubTabEditView.extend({
         subTabEditTplSelector: '#wordpressdirectory-subtab-edit-template',
 
+        render: function() {
+			wxApp.WordpressDirectorySubTabEditView.__super__.render.call( this );
+
+			var url = this.model.get('config').url;
+			$('.wx-add-wordpress-directory-select').val( url );
+        },
+
         setModelFromView: function(model) {
             if ( this.$('.wx-add-wordpress-directory-select') )
                 model.setConfig('url', this.$('.wx-add-wordpress-directory-select').find(':selected').val());
--- js/views/subtab.wordpresstag.edit.js
+++ js/views/subtab.wordpresstag.edit.js
@@ -5,6 +5,13 @@ wxApp = wxApp || {};
     wxApp.WordpressTagSubTabEditView = wxApp.SubTabEditView.extend({
         subTabEditTplSelector: '#wordpresstag-subtab-edit-template',
 
+        render: function() {
+			wxApp.WordpressTagSubTabEditView.__super__.render.call( this );
+
+			var url = this.model.get('config').url;
+			$('.wx-add-wordpress-tag-select').val( url );
+        },
+
         setModelFromView: function(model) {
             if ( this.$('.wx-add-wordpress-tag-select') )
                 model.setConfig('url', this.$('.wx-add-wordpress-tag-select').find(':selected').val());
