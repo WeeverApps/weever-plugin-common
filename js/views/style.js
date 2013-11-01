ab053703ab632a527ad94d73eb497c83cfa9c6c2
Matt Grande <matt@weeverapps.com>
2013-11-01 11:19:32 -0400

    CSS changes for saved span.

--- js/views/style.js
+++ js/views/style.js
@@ -21,7 +21,7 @@ wxApp = wxApp || {};
             // Add it to the page for two seconds, then fade it out, then remove it from the page.
             // Hide the loading gif
             var post_load = id + "_done";
-            var span = "<span id=\"" + post_load + "\">Saved.</span>";
+            var span = "<span id=\"" + post_load + "\" class=\"left inline\">Saved.</span>";
             console.log(span)
             $(loading_id).before( span );
             $( '#' + post_load ).delay( 2000 ).fadeOut( 1000 ).queue( function() { $(this).remove();} );
