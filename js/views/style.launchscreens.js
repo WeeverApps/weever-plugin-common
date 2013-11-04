52bada8985b4f00761ef0ad5b4449db5279a7977
Matt Grande <matt@weeverapps.com>
2013-11-04 14:54:30 -0500

    Image now saves as soon as it crops.

--- js/views/style.launchscreens.js
+++ js/views/style.launchscreens.js
@@ -6,22 +6,24 @@ wxApp = wxApp || {};
         el: '#launch_screen',
 
         events: {
-            'click #save_launch_screen': 'save'
+            'change .wx-theme-input': 'saveImage'
         },
 
         initialize: function() {
+            Backbone.Events.on( 'image:change', this.saveImage, this );
             this.tpl = _.template( $('#launch-screen').html() );
             this.$('.content').html( this.tpl( this.model.toJSON() ) );
         },
 
-        save: function() {
-            console.log('launch screen: save clicked');
+        saveImage: function( input ) {
+            var id = input.attr('name');
+            var src = input.val();
 
-            $('#save_launch_screen').html('Saving...');
+            id = id.replace('_load_live', '');
+            alert(id);
+            alert(src);
 
-            wxApp.design.get('launchscreen').phone            = wx.cleanUrl( $('#wx-phone_load_live').attr('src') );
-            wxApp.design.get('launchscreen').tablet           = wx.cleanUrl( $('#wx-tablet_load_live').attr('src') );
-            wxApp.design.get('launchscreen').tablet_landscape = wx.cleanUrl( $('#wx-tablet_landscape_load_live').attr('src') );
+            wxApp.design.get('launchscreen')[id] = src;
 
             // The 'design' methods of Open API is kinda strange... It 
             // expects top-level params to be JSON objects, and items within 
@@ -30,14 +32,12 @@ wxApp = wxApp || {};
             var innerParams = JSON.stringify( wxApp.design.get('launchscreen') );
             var params = { launchscreen: innerParams };
 
-            // set_launchscreen
             wx.makeApiCall('design/set_launchscreen', params, function(data) {
                 $('#save_launch_screen').html('Saved!');
                 // Wait half a second, then refresh the preview
                 // (The half-second helps ensure the server is synced)
                 setTimeout( function() { wx.refreshAppPreview(); }, 500);
             });
-
         }
 
     });
