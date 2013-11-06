888a3e611385e518350806d104e13aec1b5373c0
Matt Grande <matt@weeverapps.com>
2013-11-06 11:13:55 -0500

    Fix up for picasa albums.

--- js/views/subtab.picasaalbums.edit.js
+++ js/views/subtab.picasaalbums.edit.js
@@ -6,8 +6,10 @@ wxApp = wxApp || {};
         subTabEditTplSelector: '#picasaalbums-subtab-edit-template',
 
         setModelFromView: function(model) {
-            if ( this.$('.wx-edit-input') )
+            if ( this.$('.wx-edit-input') ) {
                 model.setConfig('user_id', this.$('.wx-edit-input').val());
+                model.setConfig('email', this.$('.wx-edit-input').val());
+            }
             return model;
         }
     });
