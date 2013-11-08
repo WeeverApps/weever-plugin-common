a698792f7e681c63029481e160c04adb0882ca14
Matt Grande <matt@weeverapps.com>
2013-11-08 10:02:08 -0500

    Redo icon saving for new format. [refs #3787]

--- js/views/container.edit.js
+++ js/views/container.edit.js
@@ -23,31 +23,22 @@ wxApp = wxApp || {};
     	},
 
     	save: function() {
-    		console.log('Save.');
-            var me = this;
-            var tabId = this.model.get('id');
-            var title = $('#container-title').val();
-            var iconId = $('input:radio[name="wx-icon"]:checked').val();
-            var numCompleted = 0;
-
-            // Prefix with e, then pad with zeros as necessary.
-            iconId = 'e' + ("000" + iconId).slice(-3);
-            // Convert from hex string, to integer, to unicode character.
-            var b16char = String.fromCharCode( parseInt( iconId, 16 ) );
-            alert( b16char );
+            var me = this,
+                tabId = this.model.get('id'),
+                title = $('#container-title').val(),
+                iconId = $('input:radio[name="wx-icon"]:checked').val(),
+                numCompleted = 0;
 
             wx.makeApiCall( 'tabs/set_tabTitle', { tab_id: tabId, tabTitle: title }, function() {
-                console.log('Title Saved');
                 me.model.set('tabTitle', title);
                 if (++numCompleted == 2) {
                     wx.rebuildApp();
                 }
             });
 
-            wx.makeApiCall( 'tabs/set_tabIcon', { tab_id: tabId, tabIcon_id: b16char }, function() {
-                console.log('Tab Icon Saved');
+            wx.makeApiCall( 'tabs/set_tabIcon', { tab_id: tabId, tabIcon_id: iconId }, function() {
                 me.model.set('tabIcon_id', null);
-                me.model.set('tabIcon', b16char);
+                me.model.set('tabIcon', iconId);
                 if (++numCompleted == 2) {
                     wx.rebuildApp();
                 }
