e910e0f3b3a036cf1970bfc9d5755938e3a4c17a
Matt Grande <matt@weeverapps.com>
2013-11-27 12:55:54 -0500

    Fix issue where Tier 4 users would see a 'trial expires soon' message.

--- js/account.js
+++ js/account.js
@@ -21,36 +21,44 @@
 
 jQuery(document).ready(function(){ 
 
-	wx.makeApiCall('account/get_expiry', {}, function(data) {
-		// data.expiry = "2013-10-17 20:00:00";
-		if (data.expiry == "0000-00-00 00:00:00") {
-			// Doesn't expire.
-			return;
+	wx.makeApiCall('account/get_tier' {}, function(data) {
+
+		if ( data.expiry == 2.1 || data.expiry == '2.1' ) {
+
+			// This user has a trial account. Check the expiration date.
+			wx.makeApiCall('account/get_expiry', {}, function(data) {
+				// data.expiry = "2013-10-17 20:00:00";
+				if (data.expiry == "0000-00-00 00:00:00") {
+					// Doesn't expire.
+					return;
+				}
+
+				var d = new Date(data.expiry);
+				var millisecondsPerDay = 86400000; //1000 * 60 * 60 * 24;
+				var millisecondsBetween = d.getTime() - (new Date()).getTime();
+				var days = millisecondsBetween / millisecondsPerDay;
+				days = Math.floor( days );
+				if (days < 0) { 
+					jQuery('#account-expired').show();
+					return;
+				}
+
+				if (days < 5) {
+					jQuery('#account-expiration-warning .secondary').addClass('alert').removeClass('secondary');
+				}
+
+				if ( days == 0 ) {
+					jQuery('#expiry-days').html( 'today!' );
+				} else if ( days == 1 ) {
+					jQuery('#expiry-days').html( 'in one day.' );
+				} else {
+					jQuery('#expiry-days').html( 'in ' + days.toString() + ' days.' );
+				}
+
+				jQuery('#account-expiration-warning').show();
+			});
 		}
 
-		var d = new Date(data.expiry);
-		var millisecondsPerDay = 86400000; //1000 * 60 * 60 * 24;
-		var millisecondsBetween = d.getTime() - (new Date()).getTime();
-		var days = millisecondsBetween / millisecondsPerDay;
-		days = Math.floor( days );
-		if (days < 0) { 
-			jQuery('#account-expired').show();
-			return;
-		}
-
-		if (days < 5) {
-			jQuery('#account-expiration-warning .secondary').addClass('alert').removeClass('secondary');
-		}
-
-		if ( days == 0 ) {
-			jQuery('#expiry-days').html( 'today!' );
-		} else if ( days == 1 ) {
-			jQuery('#expiry-days').html( 'in one day.' );
-		} else {
-			jQuery('#expiry-days').html( 'in ' + days.toString() + ' days.' );
-		}
-
-		jQuery('#account-expiration-warning').show();
 	});
 
 });
