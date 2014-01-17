3e0614d4931c3deb9414f8f0f7af3ece8450a23f
Matt Grande <matt@weeverapps.com>
2014-01-17 16:22:15 -0500

    Adding 'new page' functions. [refs #4143 and spent 3 hours on #4143]

--- js/views/subtab.wordpressaddpage.edit.js
+++ js/views/subtab.wordpressaddpage.edit.js
@@ -16,9 +16,45 @@ wxApp = wxApp || {};
         },
 
         setModelFromView: function(model) {
-            // if ( this.$('.wx-add-wordpress-page-select') )
-            //     model.setConfig('url', this.$('.wx-add-wordpress-page-select').find(':selected').val());
-            // return model;
+            
+			var content = 'CONTENT',//tinyMCE.activeEditor.getContent(), //{format : 'raw'}), //jQuery('#wx-add-content-editor').val(),
+        		name = 'tabName';	// $('.tabname or whatever.')
+
+            model.setConfig('type', 'htmlContent');
+            model.setConfig('name', name);
+            model.set('title', 'name');
+            model.set('content', 'htmlPage');
+            model.set('published', 1);
+            model.set('parent_id', null);
+            model.set('layout', 'panel');
+            model.set('tabLayout', 'list');
+
+        	jQuery.ajax({
+	            type: "POST",
+	            url: ajaxurl,
+	            async: false,
+	            data: {
+	                action: 'ajaxAddNewContent',
+	                content: content, 
+	                name: name,
+	                nonce: jQuery('input#nonce').val()
+	            },
+	            success: function(url){
+	                console.log(url);
+
+	                model.setConfig('url', url);
+	                alert('success');
+	            },
+	            error: function(v,msg){
+	                alert('error');
+	                console.log(v);
+	                console.log(msg);
+	            }
+	        });
+
+	        alert('done');
+	        return model;
+
         }
     });
 })(jQuery);
