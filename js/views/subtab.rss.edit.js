7f19115fccbc5ac45b0284639a29574b1238f87d
Matt Grande <matt@weeverapps.com>
2014-01-21 10:25:02 -0500

    RSS fix.

--- js/views/subtab.rss.edit.js
+++ js/views/subtab.rss.edit.js
@@ -6,8 +6,15 @@ wxApp = wxApp || {};
         subTabEditTplSelector: '#rss-subtab-edit-template',
 
         setModelFromView: function(model) {
-            if ( this.$('.wx-edit-input') )
-                model.setConfig('url', this.$('.wx-edit-input').val());
+        	var url = this.$('.wx-edit-input').val();
+
+            if ( url ) {
+            	if ( url.indexOf('_rss2r3s') == -1 ) {
+					url = 'http://weeverapp.com/api/v2/_rss2r3s/byUrl?url=' + encodeURIComponent(url);
+				}
+				model.setConfig("url", url);
+			}
+			
             return model;
         }
     });
