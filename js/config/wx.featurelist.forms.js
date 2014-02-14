ef26b33e36ef936d1a1de204165b90154409e6ee
Matt Grande <matt@weeverapps.com>
2014-02-14 11:21:45 -0500

    Rename files.

---
+++ js/config/wx.featurelist.forms.js
@@ -0,0 +1,8 @@
+[
+    { "featureName": "FormBuilder",         "name": "FormBuilder", "cms": ["cloud"]  },
+    { "featureName": "DocuSign",            "name": "DocuSign",    "cms": ["cloud"]  },
+    { "featureName": "WordpressAddPage",    "name": "Add Pages",   "cms": ["cloud"], "filterBy": "weever_list_show_wordpress_content", "rel": "page"         },
+    { "featureName": "WordpressContacts",   "name": "Contact"      },
+    { "featureName": "Twitter",             "name": "Twitter"      },
+    { "featureName": "Youtube",             "name": "YouTube"      }
+]
