d25cee1d4be6ef18c2f4174aa7ff9f49a77a283b
Garth Gutenberg <garth.gutenberg@gmail.com>
2014-02-24 17:01:56 -0500

    Added current user email address to Send PDF action.

--- js/views/subtab.formbuilder.edit.js
+++ js/views/subtab.formbuilder.edit.js
@@ -225,11 +225,25 @@ wxApp = wxApp || {};
 			'click .wx-form-builder-add-pagebreak'           : 'addPagebreak',
 			'click .wx-form-builder-add-docusign-signature'  : 'addDocusignSignature',
 			'click .wx-form-builder-row'                     : 'setActivePreviewElement',
+			'change .wx-form-builder-send-current-user-email': 'toggleSendEmailAddress',
 			'keyup .submit-button-text'                      : 'updateSubmitButtonText',
 			'sortable-update'                                : 'sortableUpdate',
 //			'close'                                          : 'confirmClosePopup', // Should use this if we can figure out a way to prevent a Foundation Reveal from closing
 			'click .wx-close-button'                         : 'closeConfirmation',
 			'click .wx-close-reveal-modal'                   : 'closeConfirmation'
+
+		},
+
+		toggleSendEmailAddress: function( ev ) {
+			console.log( $( ev.currentTarget ).is( ':checked' ) );
+			var $target = $( ev.currentTarget );
+			var $input = $target.closest( '.wx-form-builder-row.email' ).find( 'input.wx-form-builder-action[type="email"]' );
+			if ( $target.is( ':checked' ) ) {
+				$input.val( wx.currentUserEmail );
+			}
+			else {
+				$input.val( '' );
+			}
 
 		},
 
