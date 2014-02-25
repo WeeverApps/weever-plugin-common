7a7fd5dcc844ceb6ba98af9295be2ff480f334af
Matt Grande <matt@weeverapps.com>
2014-02-24 20:32:06 -0500

    Clean up featurelist.

--- js/config/wx.featurelist.docusign.js
+++ js/config/wx.featurelist.docusign.js
@@ -1,14 +1,8 @@
 [
-    { "featureName": "FormBuilder",         "name": "FormBuilder", "cms": ["none"] },
+    { "featureName": "FormBuilder",         "name": "FormBuilder", "cms": ["none"]  },
     { "featureName": "DocuSign",            "name": "DocuSign",    "cms": ["cloud"] },
-    { "featureName": "RSS",                 "name": "RSS feed"     },
+    { "featureName": "WordpressAddPage",    "name": "Add Pages",   "cms": ["cloud"] },
     { "featureName": "Twitter",             "name": "Twitter"      },
     { "featureName": "Youtube",             "name": "YouTube"      },
-    { "featureName": "Vimeo",               "name": "Vimeo"        },
-    { "featureName": "FacebookWall",        "name": "FB Wall"      },
-    { "featureName": "FacebookEvents",      "name": "FB Events"    },
-    { "featureName": "FacebookAlbums",      "name": "FB Photos"    },
-    { "featureName": "Flickr",              "name": "Flickr"       },
-    { "featureName": "PicasaAlbums",        "name": "Picasa"       },
-    { "featureName": "FoursquarePhotos",    "name": "Foursquare"   }
+    { "featureName": "WordpressContacts",   "name": "Contact"      }
 ]
