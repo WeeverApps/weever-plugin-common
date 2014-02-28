b1e24d4f237b8f4d64c384759bdf9fd230df0a7f
Matt Grande <matt@weeverapps.com>
2014-02-28 16:13:50 -0500

    Add tier to docusign.

--- js/config/wx.featurelist.docusign.js
+++ js/config/wx.featurelist.docusign.js
@@ -1,6 +1,6 @@
 [
     { "featureName": "FormBuilder",         "name": "FormBuilder", "cms": ["cloud"] },
-    { "featureName": "DocuSign",            "name": "DocuSign",    "cms": ["cloud"] },
+    { "featureName": "DocuSign",            "name": "DocuSign",    "cms": ["cloud"], "tierRequired": 100 },
     { "featureName": "WordpressAddPage",    "name": "Add Pages",   "cms": ["cloud"] },
     { "featureName": "Twitter",             "name": "Twitter"      },
     { "featureName": "Youtube",             "name": "YouTube"      },
